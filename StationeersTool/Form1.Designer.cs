e277bff960497e5b4929172712faf7e373b78aa2
Maciej Bregisz <maciox55@gmail.com>
2017-12-25 04:23:58 -0500

    Add project files.

---
+++ StationeersTool/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace StationeersTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imgUpload = new System.Windows.Forms.Button();
+            this.worldFileUpload = new System.Windows.Forms.Button();
+            this.saveNewWorldFile = new System.Windows.Forms.Button();
+            this.imgFileLabel = new System.Windows.Forms.Label();
+            this.worldDataFileLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.processImg = new System.Windows.Forms.Button();
+            this.debug1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgUpload
+            // 
+            this.imgUpload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.imgUpload.Location = new System.Drawing.Point(3, 389);
+            this.imgUpload.Name = "imgUpload";
+            this.imgUpload.Size = new System.Drawing.Size(75, 23);
+            this.imgUpload.TabIndex = 0;
+            this.imgUpload.Text = "Upload Img";
+            this.imgUpload.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.imgUpload.UseVisualStyleBackColor = true;
+            this.imgUpload.Click += new System.EventHandler(this.imgUpload_Click);
+            // 
+            // worldFileUpload
+            // 
+            this.worldFileUpload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.worldFileUpload.Location = new System.Drawing.Point(3, 418);
+            this.worldFileUpload.Name = "worldFileUpload";
+            this.worldFileUpload.Size = new System.Drawing.Size(135, 23);
+            this.worldFileUpload.TabIndex = 1;
+            this.worldFileUpload.Text = "Upload World File";
+            this.worldFileUpload.UseVisualStyleBackColor = true;
+            this.worldFileUpload.Click += new System.EventHandler(this.worldFileUpload_Click);
+            // 
+            // saveNewWorldFile
+            // 
+            this.saveNewWorldFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveNewWorldFile.Location = new System.Drawing.Point(800, 427);
+            this.saveNewWorldFile.Name = "saveNewWorldFile";
+            this.saveNewWorldFile.Size = new System.Drawing.Size(75, 23);
+            this.saveNewWorldFile.TabIndex = 2;
+            this.saveNewWorldFile.Text = "Save";
+            this.saveNewWorldFile.UseVisualStyleBackColor = true;
+            this.saveNewWorldFile.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // imgFileLabel
+            // 
+            this.imgFileLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.imgFileLabel.AutoSize = true;
+            this.imgFileLabel.Location = new System.Drawing.Point(84, 394);
+            this.imgFileLabel.Name = "imgFileLabel";
+            this.imgFileLabel.Size = new System.Drawing.Size(35, 13);
+            this.imgFileLabel.TabIndex = 3;
+            this.imgFileLabel.Text = "label1";
+            // 
+            // worldDataFileLabel
+            // 
+            this.worldDataFileLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.worldDataFileLabel.AutoSize = true;
+            this.worldDataFileLabel.Location = new System.Drawing.Point(144, 423);
+            this.worldDataFileLabel.Name = "worldDataFileLabel";
+            this.worldDataFileLabel.Size = new System.Drawing.Size(35, 13);
+            this.worldDataFileLabel.TabIndex = 4;
+            this.worldDataFileLabel.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(283, 281);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(502, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(373, 409);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(295, 25);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(79, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(292, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Z Index:";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // processImg
+            // 
+            this.processImg.Location = new System.Drawing.Point(292, 310);
+            this.processImg.Name = "processImg";
+            this.processImg.Size = new System.Drawing.Size(204, 81);
+            this.processImg.TabIndex = 9;
+            this.processImg.Text = "Process";
+            this.processImg.UseVisualStyleBackColor = true;
+            this.processImg.Click += new System.EventHandler(this.processImg_Click);
+            // 
+            // debug1
+            // 
+            this.debug1.AutoSize = true;
+            this.debug1.Location = new System.Drawing.Point(12, 287);
+            this.debug1.Name = "debug1";
+            this.debug1.Size = new System.Drawing.Size(35, 13);
+            this.debug1.TabIndex = 10;
+            this.debug1.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 453);
+            this.Controls.Add(this.debug1);
+            this.Controls.Add(this.processImg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.worldDataFileLabel);
+            this.Controls.Add(this.imgFileLabel);
+            this.Controls.Add(this.saveNewWorldFile);
+            this.Controls.Add(this.worldFileUpload);
+            this.Controls.Add(this.imgUpload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button imgUpload;
+        private System.Windows.Forms.Button worldFileUpload;
+        private System.Windows.Forms.Button saveNewWorldFile;
+        private System.Windows.Forms.Label imgFileLabel;
+        private System.Windows.Forms.Label worldDataFileLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button processImg;
+        private System.Windows.Forms.Label debug1;
+    }
+}
+
---
+++ StationeersTool/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Xml.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StationeersTool
+{
+    public partial class Form1 : Form
+    {
+        public Image imgFile;
+        public XDocument document;
+        public int finished, sheet, frame, lastID;
+        public long ownerID;
+        public XElement root, things, thingsaved;
+        public int x, y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imgUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp; *.png" ;
+            if ( fileDialog.ShowDialog() == DialogResult.OK) {
+                try
+                {
+                    imgFile = new Bitmap(fileDialog.FileName);
+                    pictureBox1.Image = imgFile;
+                    imgFileLabel.Text = fileDialog.FileName;
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            
+
+        }
+
+ 
+
+        private void worldFileUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "XML File(*.xml)|*.xml";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    document = XDocument.Load(fileDialog.FileName);
+                    worldDataFileLabel.Text = fileDialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void readXML() {
+            root = document.Root;
+            things = root.Elements("Things").First();
+            thingsaved = things.Elements("ThingSaveData").Last();
+
+            lastID = Int32.Parse(thingsaved.Elements("ReferenceId").First().Value);
+            ownerID = Int64.Parse(thingsaved.Elements("OwnerSteamId").First().Value);
+
+
+
+            //things.Add(new XElement("ThingSaveData"));
+
+            //foreach (var thing in root) {
+            //richTextBox1.Text += root;
+            //}
+            //var structures = from things in root.Elements("Things") where (string)things.Element("ThingSaveData") select things;
+
+        }
+
+        public void readPixels(Image img)
+        {
+            lastID++;
+            try
+            {
+                finished = 0;
+                sheet = 0;
+                frame = 0;
+                for (int i = 1; i < img.Width+1; i++)
+                {
+                    for (int j = 1; j < img.Height+1; j++)
+                    {
+                        Bitmap bitmap = new Bitmap(img);
+
+                        x = i + (i - 1);
+                        y = j + (j - 1);
+
+                        //Console.WriteLine("(X:" + x + " : Y:" + y + " J: "+j+")");
+
+                        Color pixel = bitmap.GetPixel(i-1, j-1);
+                        //Finished Frame
+                        if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0)
+                        {
+                            // Console.WriteLine(" Finished Frame at: " + "(" + i + "," + j + ")");
+                            finished++;
+                            lastID = lastID + 1;
+                            XNamespace ns = "http://www.w3.org/2001/XMLSchema-instance";
+                            things.Add(new XElement("ThingSaveData",
+                                new XAttribute(ns + "type","StructureSaveData"),
+                                new XElement("ReferenceId",lastID),
+                                new XElement("PrefabName", "StructureFrameIron"),
+                                new XElement("CustomName"),
+                                new XElement("WorldPosition",
+                                    new XElement("x",x),
+                                    new XElement("y", numericUpDown1.Value),
+                                    new XElement("z", y)),
+                                new XElement("WorldRotation", 
+                                    new XElement("x",0),
+                                    new XElement("y", 0),
+                                    new XElement("z", 0),
+                                    new XElement("w", 1),
+                                    new XElement("eulerAngles",
+                                        new XElement("x", 0),
+                                        new XElement("y", 0),
+                                        new XElement("z", 0))),
+                                new XElement("States"),
+                                new XElement("IsCustomName","false"),
+                                new XElement("CustomColorIndex",-1),
+                                new XElement("OwnerSteamId", ownerID),
+                                new XElement("Reavents"),
+                                new XElement("Indestructable","false"),
+                                new XElement("CurrentBuildState",2),
+                                new XElement("MothershipReferenceId", 0)
+                                ));
+                        }
+                        //1 Sheet Frame
+                        if (pixel.R == 85 && pixel.G == 85 && pixel.B == 85)
+                        {
+                            sheet++;
+                            lastID = lastID + 1;
+                            XNamespace ns = "http://www.w3.org/2001/XMLSchema-instance";
+                            things.Add(new XElement("ThingSaveData",
+                                new XAttribute(ns + "type", "StructureSaveData"),
+                                new XElement("ReferenceId", lastID),
+                                new XElement("PrefabName", "StructureFrameIron"),
+                                new XElement("CustomName"),
+                                new XElement("WorldPosition",
+                                    new XElement("x", x),
+                                    new XElement("y", numericUpDown1.Value),
+                                    new XElement("z", y)),
+                                new XElement("WorldRotation",
+                                    new XElement("x", 0),
+                                    new XElement("y", 0),
+                                    new XElement("z", 0),
+                                    new XElement("w", 1),
+                                    new XElement("eulerAngles",
+                                        new XElement("x", 0),
+                                        new XElement("y", 0),
+                                        new XElement("z", 0))),
+                                new XElement("States"),
+                                new XElement("IsCustomName", "false"),
+                                new XElement("CustomColorIndex", -1),
+                                new XElement("OwnerSteamId", ownerID),
+                                new XElement("Reavents"),
+                                new XElement("Indestructable", "false"),
+                                new XElement("CurrentBuildState", 1),
+                                new XElement("MothershipReferenceId", 0)
+                                ));
+
+
+
+                        }
+                        //Just Frame
+                        if (pixel.R == 170 && pixel.G == 170 && pixel.B == 170)
+                        {
+                            frame++;
+                            lastID = lastID + 1;
+                            XNamespace ns = "http://www.w3.org/2001/XMLSchema-instance";
+                            things.Add(new XElement("ThingSaveData",
+                                new XAttribute(ns + "type", "StructureSaveData"),
+                                new XElement("ReferenceId", lastID),
+                                new XElement("PrefabName", "StructureFrameIron"),
+                                new XElement("CustomName"),
+                                new XElement("WorldPosition",
+                                    new XElement("x", x),
+                                    new XElement("y", numericUpDown1.Value),
+                                    new XElement("z", y)),
+                                new XElement("WorldRotation",
+                                    new XElement("x", 0),
+                                    new XElement("y", 0),
+                                    new XElement("z", 0),
+                                    new XElement("w", 1),
+                                    new XElement("eulerAngles",
+                                        new XElement("x", 0),
+                                        new XElement("y", 0),
+                                        new XElement("z", 0))),
+                                new XElement("States"),
+                                new XElement("IsCustomName", "false"),
+                                new XElement("CustomColorIndex", -1),
+                                new XElement("OwnerSteamId", ownerID),
+                                new XElement("Reavents"),
+                                new XElement("Indestructable", "false"),
+                                new XElement("CurrentBuildState", 0),
+                                new XElement("MothershipReferenceId", 0)
+                                ));
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Make sure you have your structure img and world save file selected! " + ex);
+            }
+            
+
+            debug1.Text ="Finished: " + finished + " 1 Sheet: " + sheet + " Just Frame: " + frame;
+        }
+
+        private void processImg_Click(object sender, EventArgs e)
+        {
+            readXML();
+            readPixels(imgFile);
+            richTextBox1.Text += root;
+        }
+    }
+}
+
+
+/*
+ * <ThingSaveData xsi:type="StructureSaveData">
+      <ReferenceId>97</ReferenceId>
+      <PrefabName>StructureFrameIron</PrefabName>
+      <CustomName />
+      <WorldPosition>
+        <x>29</x>
+        <y>1</y>
+        <z>-5</z>
+      </WorldPosition>
+      <WorldRotation>
+        <x>0</x>
+        <y>0</y>
+        <z>0</z>
+        <w>1</w>
+        <eulerAngles>
+          <x>0</x>
+          <y>0</y>
+          <z>0</z>
+        </eulerAngles>
+      </WorldRotation>
+      <States />
+      <IsCustomName>false</IsCustomName>
+      <CustomColorIndex>-1</CustomColorIndex>
+      <OwnerSteamId>76561198008916569</OwnerSteamId>
+      <Reagents />
+      <Indestructable>false</Indestructable>
+      <CurrentBuildState>2</CurrentBuildState>
+      <MothershipReferenceId>0</MothershipReferenceId>
+    </ThingSaveData>
+ * 
+ * 
+ * 
+ */
